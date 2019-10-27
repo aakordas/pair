8289fe8aaded041cec41b14264b794dc941d3c97
Alkis Kordas <sdi1400249@di.uoa.gr>
2019-10-27 12:36:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# pair
+Inspired by C++'s `std:pair`,  one can create pairs with elements of the same kind (i.e. [1, 2]) or of different kind (i.e. ["a", 1]). Besides different construction functions, an equality function is also provided.
+
+## Installation
+
+To install, type 
+```
+go get github.com/aakordas/pair
+```
+
+## Usage
+
+To use it in a program, do 
+```
+import "github.com/aakordas/pair"
+```
---
+++ pair.go
@@ -0,0 +1,39 @@
+/* Inspired by C++ pairs. Currently it has rather basic functionality. One can
+ * create pairs with elements of different kinds just fine. (a, b) != (b, a)
+ */
+
+package pair
+
+type Pair []interface{}
+
+// Create creates and returns a new pair.
+func Create() (p Pair) {
+	p = make([]interface{}, 2, 2)
+
+	return p
+}
+
+// Make makes a pair with the provided arguments as values
+func (p Pair) Make(e1, e2 interface{}) {
+	p[0] = e1
+	p[1] = e2
+}
+
+// NewPair creates and initializes a new pair and returns it.
+func NewPair(e1, e2 interface{}) (p Pair) {
+	p = make([]interface{}, 2, 2)
+
+	p[0] = e1
+	p[1] = e2
+
+	return p
+}
+
+// Equals checks if two pairs are equal
+func (p Pair) Equals(pa Pair) (bool) {
+	if p[0] == pa[0] && p[1] == pa[1] {
+		return true
+	}
+
+	return false
+}
